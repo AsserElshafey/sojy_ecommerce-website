8f0a8b3a5e627daa25e91c3b285e54c3ea2f4088
= <=>
2023-12-05 19:39:00 +0200

    login and register routes

--- web_app/auth.py
+++ web_app/auth.py
@@ -1,10 +1,27 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login', strict_slashes=False)
+@auth.route('/login', strict_slashes=False, methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        print(email)
+        print(password)
+    return render_template("login.html")
+
+
+@auth.route('/register', strict_slashes=False, methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form.get('reg_email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        print(email)
+        print(password1)
+        print(password2)
     return render_template("login.html")
 
 
