5511e64b6d9624ce5d4393bb47ccf5ebfec59163
= <=>
2023-12-06 23:29:43 +0200

    added product model and removed blog from nav

--- web_app/models.py
+++ web_app/models.py
@@ -6,3 +6,10 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(256))
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    quantity = db.Column(db.Integer)
+    product_details = db.Column(db.String(512))
