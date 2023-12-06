2c7a55f258e8b4d6ec17f8106afc75ca75818855
= <=>
2023-12-06 13:31:06 +0200

    login and registration page complete

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 
@@ -14,9 +15,18 @@ def create_app():
     from .auth import auth
 
     from . import models
+    from .models import User
 
     with app.app_context():
         db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'view.home'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
--- web_app/auth.py
+++ web_app/auth.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from .models import User
 from . import db
+from flask_login import login_user, login_required, current_user, logout_user
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -12,8 +13,18 @@ def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-        print(email)
-        print(password)
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Login Successful", category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash("Your password is incorrect.", category='error')
+        else:
+            flash("This user doesn't exist")
+            return redirect("https://www.youtube.com/watch?v=nEf2ML7wkBE", code=302)
+
     return render_template("login.html")
 
 
@@ -24,19 +35,28 @@ def register():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        new_user = User(
-            email=email, password=generate_password_hash(password1, method='pbkdf2:sha256'))
-        db.session.add(new_user)
-        db.session.commit()
-        print('account created!')
+        user = User.query.filter_by(email=email).first()
 
-        print(email)
-        print(password1)
-        print(password2)
-        return redirect(url_for('views.home'))
+        if password1 != password2:
+            flash('Passwords mismatch', category='error')
+        elif len(password1) < 8:
+            flash('Password must be atleast 8 characters', category='error')
+        elif user:
+            flash('Dis nigga already exists')
+        else:
+            # Create new user
+            new_user = User(
+                email=email, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Account created!", category='success')
+            login_user(user, remember=True)
+            return redirect(url_for('views.home'))
     return render_template("login.html")
 
 
 @auth.route('/logout', strict_slashes=False)
+@login_required
 def logout():
-    return "logout"
+    logout_user()
+    return render_template("login.html")
